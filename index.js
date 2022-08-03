bb745ab5ffdde26463fce2f434fbfc5e82b572d3
Punit9349 <puneetgarg9349@gmail.com>
2022-08-03 15:50:10 +0530

    commit

--- index.js
+++ index.js
@@ -4,17 +4,14 @@ var cors= require('cors')
 
 connectToMongo();
 const app = express()
-const port = 5001
+const port = 5001 || process.env.Port;
 
 app.use(cors())
 app.use(express.json())
 
 // Available routes
 app.use('/api/auth', require('./routes/auth'))
-// app.use('/api/notes', require('./routes/notes'))
-
 app.use('/api/contact', require('./routes/contact'))
-// app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`myNotebook backend listening on port ${port}`)
--- routes/contact.js
+++ routes/contact.js
@@ -1,37 +1,27 @@
 const express=require('express');
 const router= express.Router();
 const Contact= require('../models/Contact');
-// const { body, validationResult } = require('express-validator');
+const { body } = require('express-validator');
 
 // ROUTE 2: Add a new Note using: Post "/api/contact/contactdetails"
 router.post('/contactdetails', [
     // body('name','Enter a valid title').isLength({ min: 3 }),
-    // body('message','Description must be of atleast 5 characters').isLength({ min: 5 }), 
+    body('phone','Phone no must be of 10 characters').isLength({ min: 10, max:10 }), 
 ] , async (req, res)=>{
 
     try {
         
+    console.log(req.body);
+    const contact= await Contact.create({
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        message:req.body.message
+      });
 
-     const {name, email, phone }=req.body;
-     // If there are errors, return Bad requests and errors. 
-    //  const errors = validationResult(req);
-    //  if (!errors.isEmpty()) {
-    //    return res.status(400).json({ errors: errors.array() });
-    //  }
-    
-    const contact= new Contact({
-        name, email, phone: req.body
-    })
-    // const contact= await Contact.create({
-    //     name: req.body.name,
-    //     email: req.body.email,
-    //     phone: req.body.phone,
-    //     // message:req.body.message
-    //   });
-
-
-     const savedContact= await contact.save(); 
-     
+      const savedContact= await contact.save(); 
+      
+      console.log(savedContact);
     
     res.json(savedContact)
 
