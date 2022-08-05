cfe45256561900c815c9723b4e5ef0f984d771b7
Punit9349 <puneetgarg9349@gmail.com>
2022-08-05 15:01:20 +0530

    improving contact us

--- routes/contact.js
+++ routes/contact.js
@@ -1,13 +1,20 @@
 const express=require('express');
 const router= express.Router();
 const Contact= require('../models/Contact');
-const { body } = require('express-validator');
+const { body , validationResult} = require('express-validator');
 
 // ROUTE 2: Add a new Note using: Post "/api/contact/contactdetails"
 router.post('/contactdetails', [
-    // body('name','Enter a valid title').isLength({ min: 3 }),
-    body('phone','Phone no must be of 10 characters').isLength({ min: 10, max:10 }), 
+    body('name','Enter a valid name').isLength({ min: 3 }),
+    body('phone','Phone no must be of 10 characters').isLength({ min: 10, max: 10 }), 
 ] , async (req, res)=>{
+
+  let success=false;
+  // If there are errors, return Bad requests and errors. 
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ success,errors: errors.array() });
+  }
 
     try {
         
@@ -19,11 +26,12 @@ router.post('/contactdetails', [
         message:req.body.message
       });
 
+
       const savedContact= await contact.save(); 
       
       console.log(savedContact);
     
-    res.json(savedContact)
+      res.json(savedContact)
 
 
  } catch (error) {
